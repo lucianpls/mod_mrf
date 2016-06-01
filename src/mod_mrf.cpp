4d173982534675a39a4e50f1fbb5fd30b17d43c0
Lucian Plesea <lucianpls@gmail.com>
2016-06-01 15:29:35 -0700

    Basic framework

--- src/mod_mrf.cpp
+++ src/mod_mrf.cpp
@@ -6,7 +6,53 @@
 
 #include "mod_mrf.h"
 
+static void *create_dir_config(apr_pool_t *p, char *dummy)
+{
+    mrf_conf *c =
+        (mrf_conf *)apr_pcalloc(p, sizeof(mrf_conf));
+    return c;
+}
+
+static const char *mrf_file_set(cmd_parms *cmd, void *dconf, const char *arg)
+{
+    mrf_conf *c = (mrf_conf *)dconf;
+    c->the_mrf = apr_pstrdup(cmd->pool, arg);
+    // Should parse it here and initialize the configuration structure
+    return NULL;
+}
+
+static const command_rec mrf_cmds[] =
+{
+    AP_INIT_TAKE1(
+    "MRF_FILE",
+    CMD_FUNC mrf_file_set, // Callback
+    0, // Self-pass argument
+    ACCESS_CONF, // availability
+    "Where is the MRF metadata file"
+    ),
+
+    { NULL }
+};
+
+static int handler(request_rec *r)
+{
+    mrf_conf *cfg = (mrf_conf *)ap_get_module_config(r->per_dir_config, &mrf_module);
+
+    return DECLINED;
+}
+
+static void mrf_register_hooks(apr_pool_t *p)
+
+{
+    ap_hook_handler(handler, NULL, NULL, APR_HOOK_FIRST);
+}
+
 module AP_MODULE_DECLARE_DATA mrf_module = {
     STANDARD20_MODULE_STUFF,
-    0, 0, 0, 0, 0, 0
+    create_dir_config, 
+    0, // No dir_merge
+    0, // No server_config
+    0, // No server_merge
+    mrf_cmds, // configuration directives
+    mrf_register_hooks // processing hooks
 };
--- src/mod_mrf.h
+++ src/mod_mrf.h
@@ -6,7 +6,25 @@
 
 #if !defined(MOD_MRF_H)
 #include <httpd.h>
+#include <http_protocol.h>
 #include <http_config.h>
+#include <http_core.h>
+#include <http_main.h>
+#include <http_request.h>
+#include <http_log.h>
+
+#include <apr.h>
+#include <apr_lib.h>
+#include <apr_pools.h>
+#include <apr_strings.h>
+#include <apr_tables.h>
+#include <apr_uri.h>
+#include <apr_portable.h>
+
+#define APR_WANT_STRFUNC
+#define APR_WANT_MEMFUNC
+#define APR_WANT_BYTEFUNC
+#include <apr_want.h>
 
 #if defined(WIN32)
 #define CMD_FUNC (cmd_func)
@@ -15,6 +33,10 @@
 #if defined(APLOG_USE_MODULE)
 APLOG_USE_MODULE(mrf);
 #endif
+
+typedef struct {
+    char *the_mrf;     // The mrf file name
+} mrf_conf;
 
 extern module AP_MODULE_DECLARE_DATA mrf_module;
 
